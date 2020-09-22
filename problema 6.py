ea33ad9fd5278bed8598c9dcf93d65925b2d4d6c
stefanidadarii <70660311+stefanidadarii@users.noreply.github.com>
2020-09-22 20:49:15 +0300

    Update problema 6.py

--- problema 6.py
+++ problema 6.py
@@ -1,9 +1,5 @@
 n = 2378
-print('Ultima cifra a numarului n = ', n%10)
-print('Penultima cifra a numarului n = ', n//10%10)
-print('Catul si restul impartirii numarului n la 9 = ', n//9, 'si', n%9)
-print('Suma cifrelor a numarului n = ', (n//1000) + (n//100)%10 + (n//10)%10 + n%10)
-
-
-
-
+print('Ultima cifra a numarului = ', n%10)
+print('Penultima cifra a numarului = ', n//10%10)
+print('Catul si restul impartirii numarului la 9 = ', n//9, 'si', n%9)
+print('Suma cifrelor a numarului = ', (n//1000) + (n//100)%10 + (n//10)%10 + n%10)
